06d44e707912e7f848f6a3d0f8182408aa6d3c6c
Charles J. Law <charles.law@cfa.harvard.edu>
2021-09-08 19:02:36 -0400

    Update resize.bash

--- images/pub_images/resize.bash
+++ images/pub_images/resize.bash
@@ -1,23 +1,21 @@
-convert MAPS_III_Law.png -resize 33% MAPS_III_Law_thumb.png     
+convert MAPS_I_Oberg.png -resize 75% MAPS_I_Oberg_thumb.png
 convert MAPS_II_Czekala.png -resize 33% MAPS_II_Czekala_thumb.png
-convert MAPS_IV_Law.png -resize 33% MAPS_IV_Law_thumb.png
-convert MAPS_IX_Ilee.png -resize 33% MAPS_IX_Ilee_thumb.png
-convert MAPS_I_Oberg.png -resize 33% MAPS_I_Oberg_thumb.png
+convert MAPS_III_Law.png -resize 33% MAPS_III_Law_thumb.png     
+convert MAPS_IV_Law.png -resize 50% MAPS_IV_Law_thumb.png
+convert MAPS_IX_Ilee.png -resize 75% MAPS_IX_Ilee_thumb.png
 convert MAPS_VIII_Alarcon.png -resize 33% MAPS_VIII_Alarcon_thumb.png
 convert MAPS_VII_Bosman.png -resize 33% MAPS_VII_Bosman_thumb.png
 convert MAPS_VI_Guzman.png -resize 33% MAPS_VI_Guzman_thumb.png
-convert MAPS_V_Zhang.png -resize 33% MAPS_V_Zhang_thumb.png
-convert MAPS_XIII_Aikawa.png -resize 33% MAPS_XIII_Aikawa_thumb.png
 convert MAPS_XII_LeGal.png -resize 33% MAPS_XII_LeGal_thumb.png
-convert MAPS_XIV_Sierra.png -resize 33% MAPS_XIV_Sierra_thumb.png
-convert MAPS_XIX_Huang.png -resize 33% MAPS_XIX_Huang_thumb.png
-convert MAPS_XI_Bergner.png -resize 33% MAPS_XI_Bergner_thumb.png
+convert MAPS_XIV_Sierra.png -resize 75% MAPS_XIV_Sierra_thumb.png
+convert MAPS_XIX_Huang.png -resize 75% MAPS_XIX_Huang_thumb.png
+convert MAPS_XI_Bergner.png -resize 75% MAPS_XI_Bergner_thumb.png
 convert MAPS_XVII_Calahan.png -resize 33% MAPS_XVII_Calahan_thumb.png
 convert MAPS_XVI_Booth.png -resize 33% MAPS_XVI_Booth_thumb.png
 convert MAPS_XV_Bosman.png -resize 33% MAPS_XV_Bosman_thumb.png
 convert MAPS_XX_Schwarz.png -resize 33% MAPS_XX_Schwarz_thumb.png
-convert MAPS_X_Cataldi.png -resize 33% MAPS_X_Cataldi_thumb.png
 
 cp MAPS_XVIII_Teague.png MAPS_XVIII_Teague_thumb.png
-
-
+cp MAPS_V_Zhang.png MAPS_V_Zhang_thumb.png
+cp MAPS_XIII_Aikawa.png MAPS_XIII_Aikawa_thumb.png
+cp MAPS_X_Cataldi.png MAPS_X_Cataldi_thumb.png
