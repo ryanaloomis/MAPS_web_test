aa5e4bdfe2f80c488e5dca171dc19fd0ad9b2a8f
Charles J. Law <charles.law@cfa.harvard.edu>
2021-09-09 11:49:44 -0400

    Update resize.bash

--- images/pub_images/resize.bash
+++ images/pub_images/resize.bash
@@ -3,7 +3,6 @@ convert MAPS_II_Czekala.png -resize 33% MAPS_II_Czekala_thumb.png
 convert MAPS_III_Law.png -resize 33% MAPS_III_Law_thumb.png     
 convert MAPS_IV_Law.png -resize 50% MAPS_IV_Law_thumb.png
 convert MAPS_IX_Ilee.png -resize 75% MAPS_IX_Ilee_thumb.png
-convert MAPS_VIII_Alarcon.png -resize 33% MAPS_VIII_Alarcon_thumb.png
 convert MAPS_VII_Bosman.png -resize 33% MAPS_VII_Bosman_thumb.png
 convert MAPS_VI_Guzman.png -resize 33% MAPS_VI_Guzman_thumb.png
 convert MAPS_XII_LeGal.png -resize 33% MAPS_XII_LeGal_thumb.png
@@ -19,3 +18,4 @@ cp MAPS_XVIII_Teague.png MAPS_XVIII_Teague_thumb.png
 cp MAPS_V_Zhang.png MAPS_V_Zhang_thumb.png
 cp MAPS_XIII_Aikawa.png MAPS_XIII_Aikawa_thumb.png
 cp MAPS_X_Cataldi.png MAPS_X_Cataldi_thumb.png
+cp MAPS_VIII_Alarcon.png MAPS_VIII_Alarcon_thumb.png
